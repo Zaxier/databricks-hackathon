41381bd485c09d45a764fd997913884f75541cd3
Zaxier <Zaxier>
2022-06-21 21:47:12 +0000

    changed team names

--- Copy Data to DBFS.py
+++ Copy Data to DBFS.py
@@ -2,5 +2,5 @@
 def copy_repo_data_to_filestore(target_dir_name):
   dbutils.fs.cp(f'file:{os.getcwd()}/data', f'dbfs:/FileStore/hackathon/{target_dir_name}', recurse=True)
   
-for team_name in ['delta', 'spark', 'mlflow', 'python', 'sql', 'scala']:
+for team_name in ['rectifier', 'transformer', 'renewables', 'power', 'energy', 'high_voltage']:
   copy_repo_data_to_filestore(team_name)
