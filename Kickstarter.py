53e2b54f73a2f20c632f647c89ea0149fa1abc80
Zaxier <Zaxier>
2022-06-22 01:19:37 +0000

    updated

--- Kickstarter.py
+++ Kickstarter.py
@@ -1,41 +1,13 @@
 # Databricks notebook source
 def load_csv_to_spark_dataframe(path):
   return spark.read.format('csv').option("inferSchema" , "true").option('header', 'true').load(path)
-  
+    
 def save_overwrite_delta_table(df, db_name, table_name, drop_table=False):
   if drop_table:
     spark.sql(f"drop table {db_name}.{table_name}")
   df.write.format('delta').option("mergeSchema", "true").mode('overwrite').saveAsTable(f'{db_name}.{table_name}')
   
-def kickstarter_from_repo(config, drop_cascade=True):
-  if drop_cascade:
-    print(f"Dropping {config['db_name']} database")
-    spark.sql(f"drop database {config['db_name']} cascade;")
-  
-  # Create database
-  spark.sql(f"CREATE DATABASE IF NOT EXISTS {config['db_name']};")
-
-  # Load files to database
-  file_list = ['metre_predictions.csv', 
-               'metre_readings.csv', 
-               'ref_stream_sensor.csv', 
-               'ref_stream.csv', 
-               'temp_readings.csv', 
-               'temp_predictions.csv']
-
-  
-  for file in file_list:
-    df = load_csv_to_spark_dataframe(f'file:{os.getcwd()}/data/{file}')
-    table_name = file.replace('.csv', '')
-    print(f'Loading {file} to {db_name}.{table_name}')
-    save_overwrite_delta_table(df, db_name, table_name)
-    
-  spark.sql(f'show tables in {db_name}').show()
-  
-    
-def kickstarter_from_dbfs(dbfs_path):
-  
-  # Load files to database
+def kickstarter_from_dbfs_to_spark_dfs(dbfs_path):
   file_list = ['metre_predictions.csv', 
                'metre_readings.csv', 
                'ref_stream_sensor.csv', 
@@ -48,12 +20,47 @@ def kickstarter_from_dbfs(dbfs_path):
     df = load_csv_to_spark_dataframe(f'{dbfs_path}/{file}')
     dataframe_dict[file.replace('.csv', '')] = df
   return dataframe_dict
+
+def kickstarter_from_dbfs_to_db(dbfs_path, team_name, drop_cascade=False):
+  db_name = f'{team_name}_db'
+  
+  if drop_cascade:
+    spark.sql(f"drop database {team_name} cascade;")
+  
+  spark.sql(f"create database if not exists {db_name}")
+    
+  file_list = ['metre_predictions.csv', 
+               'metre_readings.csv', 
+               'ref_stream_sensor.csv', 
+               'ref_stream.csv', 
+               'temp_readings.csv', 
+               'temp_predictions.csv']
+
+  
+  for file in file_list:
+    df = load_csv_to_spark_dataframe(f'{dbfs_path}/{file}')
+    table_name = file.replace('.csv', '')
+    print(f'Loading {file} to {team_name}_db.{table_name}')
+    save_overwrite_delta_table(df, f'{team_name}_db', table_name)
+    
+  spark.sql(f'show tables in {db_name}').show()
   
 
 # COMMAND ----------
 
+# DBTITLE 1,Load datasets to your team's database
 team_name = 'transformer'
-dataframe_dict = kickstarter_from_dbfs(f'dbfs:/FileStore/hackathon/{team_name}')
+dbfs_path = f'dbfs:/FileStore/hackathon/{team_name}'
+drop_database = False
+
+kickstarter_from_dbfs_to_db(dbfs_path, team_name, drop_database)
+
+# COMMAND ----------
+
+# DBTITLE 1,Load datasets to spark dataframes
+team_name = 'transformer'
+dbfs_path = f'dbfs:/FileStore/hackathon/{team_name}'
+dataframe_dict = kickstarter_from_dbfs_to_spark_dfs()
 
 metre_readings =    dataframe_dict['metre_readings']
 metre_predictions = dataframe_dict['metre_predictions']
--- tips.py
+++
@@ -1,7 +0,0 @@
-# Databricks notebook source
-from pyspark.sql.functions import to_timestamp, from_unixtime
-
-readings = (
-  readings.withColumn("timeEnd",to_timestamp("timeEnd"))
-  .withColumn("timeRecorded", to_timestamp("timeRecorded"))
-  .withColumn("metric_0", readings.metric_0.cast("float")))
